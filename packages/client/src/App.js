b1a9715c015be8f3783d91006cf6edde39f1f484
akira.nieda <akira.nieda@GX2191.local>
2024-09-01 22:41:57 +0900

    finished by 4.6

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './styles/App.css';
 
+import { useEffect, useState } from 'react'
+
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = 'あなたのTwitterのハンドルネームを貼り付けてください';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -10,10 +12,56 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  //ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+  //ページがロードされたときに useEffect()内の関数が呼び出されます。
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum){
+        alert("Connect your MetaMask account.");
+        return;
+      }
+      const permissions = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Connected", permissions[0]);
+      setCurrentAccount(permissions[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   // renderNotConnectedContainer メソッドを定義します。
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <button className="cta-button cunnect-wallet-button">
+      If you choose image, you can mint your NFT
     </button>
   );
 
@@ -23,7 +71,11 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">あなただけの特別な NFT を Mint しよう💫</p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+              renderNotConnectedContainer()
+          ) : (
+              renderConnectedContainer()
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
